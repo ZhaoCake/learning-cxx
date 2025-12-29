275596cd857e5cd56a11b9b1aed245ae754e873b
cake <zhaocake@foxmail.com>
2025-12-29 22:53:30 +0800

    Revert "bug(exercise): finished exercise 30,31."
    This reverts commit fea8320806af81e9321563a225a938bc5dbc1970.

--- exercises/31_std_shared_ptr/main.cpp
+++ exercises/31_std_shared_ptr/main.cpp
@@ -1,8 +1,6 @@
 ﻿#include "../exercise.h"
 #include <memory>
 
-#include <iostream>
-
 // READ: `std::shared_ptr` <https://zh.cppreference.com/w/cpp/memory/shared_ptr>
 // READ: `std::weak_ptr` <https://zh.cppreference.com/w/cpp/memory/weak_ptr>
 
@@ -12,45 +10,36 @@ int main(int argc, char **argv) {
     std::shared_ptr<int> ptrs[]{shared, shared, shared};
 
     std::weak_ptr<int> observer = shared;
-    std::cerr << "step0 " << observer.use_count() << "\n";
-    ASSERT(observer.use_count() == 4, "");
+    ASSERT(observer.use_count() == ?, "");
 
     ptrs[0].reset();
-    std::cerr << "step1 " << observer.use_count() << "\n";
-    ASSERT(observer.use_count() == 3, "");
+    ASSERT(observer.use_count() == ?, "");
 
     ptrs[1] = nullptr;
-    std::cerr << "step2 " << observer.use_count() << "\n";
-    ASSERT(observer.use_count() == 2, "");
+    ASSERT(observer.use_count() == ?, "");
 
     ptrs[2] = std::make_shared<int>(*shared);
-    std::cerr << "step3 " << observer.use_count() << "\n";
-    ASSERT(observer.use_count() == 1, "");
+    ASSERT(observer.use_count() == ?, "");
 
     ptrs[0] = shared;
     ptrs[1] = shared;
     ptrs[2] = std::move(shared);
-    std::cerr << "step4 " << observer.use_count() << "\n";
-    ASSERT(observer.use_count() == 3, "");
+    ASSERT(observer.use_count() == ?, "");
 
     std::ignore = std::move(ptrs[0]);
     ptrs[1] = std::move(ptrs[1]);
     ptrs[1] = std::move(ptrs[2]);
-    std::cerr << "step5 " << observer.use_count() << "\n";
-    ASSERT(observer.use_count() == 1, "");
+    ASSERT(observer.use_count() == ?, "");
 
     shared = observer.lock();
-    std::cerr << "step6 " << observer.use_count() << "\n";
-    ASSERT(observer.use_count() == 2, "");
+    ASSERT(observer.use_count() == ?, "");
 
     shared = nullptr;
     for (auto &ptr : ptrs) ptr = nullptr;
-    std::cerr << "step7 " << observer.use_count() << "\n";
-    ASSERT(observer.use_count() == 0, "");
+    ASSERT(observer.use_count() == ?, "");
 
     shared = observer.lock();
-    std::cerr << "step8 " << observer.use_count() << "\n";
-    ASSERT(observer.use_count() == 0, "");
+    ASSERT(observer.use_count() == ?, "");
 
     return 0;
 }
