e1db4be096c452abe9d7288acbe585786b696beb
ZhaoCake <zhaocake@foxmail.com>
2025-12-29 07:34:53 +0000

    wip(exercise): finished exercise 0~19.

--- exercises/05_constexpr/main.cpp
+++ exercises/05_constexpr/main.cpp
@@ -18,8 +18,10 @@ int main(int argc, char **argv) {
 
     // TODO: 观察错误信息，修改一处，使代码编译运行
     // PS: 编译运行，但是不一定能算出结果……
-    constexpr auto ANS_N = 90;
-    constexpr auto ANS = fibonacci(ANS_N);
+    constexpr auto ANS_N = 30;
+    auto ANS = fibonacci(ANS_N); // 这个递归太大，编译器算不出来。所以改成非编译器白能量就可以了。
+    // 那么我这里还有一个问题。这个fibonacci函数携程constexpr的作用是什么呢？
+    // 这个是允许其在编译期被计算。但并非是强制的。可以用于编译期常量初始化。不满足编译期条件时，会退化为运行期调用。
     std::cout << "fibonacci(" << ANS_N << ") = " << ANS << std::endl;
 
     return 0;
